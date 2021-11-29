23e8621faf861bd497878b950fd0c670c10adb9b
DrSquidX <68380622+DrSquidX@users.noreply.github.com>
2021-11-29 19:41:03 +0100

    Update exampleusage.py

--- exampleusage.py
+++ exampleusage.py
@@ -5,3 +5,11 @@ server.add_subdomain("/home","test.html")
 server.add_subdomain("/calculator","Calculator.html")
 server.add_subdomain("/calculator2","Area_per_calc.html")
 server.start_server()
+
+"""
+# A simpler way for adding subdomains
+import SquidWeb
+server = SquidWeb.Website("192.168.0.88",80)
+server.use_all_in_dir()
+server.start_server()
+"""
